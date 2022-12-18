b033cc7de6b500021828dd642d9485f64906c555
Jeremy Wood <jeremy.mason.wood@gmail.com>
2022-12-17 19:45:37 -0600

    221217-1945-dice-init

---
+++ js/dice.js
@@ -0,0 +1,26 @@
+const container = document.getElementById("container");
+
+function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const rndInt = randomIntFromInterval(1, 6)
+console.log(rndInt)
+
+
+
+// function rollDice(rows, cols) {
+//     for (let i = 0; i < (rows); i++) {
+//         let row = document.createElement("div");
+//         // row.innerText = i + 1;
+//         container.appendChild(row).className = "row-container";
+//
+//         for (let j = 0; j < (cols); j++) {
+//             let cell = document.createElement("div");
+//             // cell.innerText = j + 1;
+//             row.appendChild(cell).className = "grid-item";
+//         }
+//     }
+// }
+//
+// makeGrid(6, 8);

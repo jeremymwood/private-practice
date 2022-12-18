5da040e92cce37450c01d1ef9dbb94c1ce7a3bc9
Jeremy Wood <jeremy.mason.wood@gmail.com>
2022-12-18 09:29:26 -0600

    221218-0930-progress

--- js/dice.js
+++ js/dice.js
@@ -1,44 +1,20 @@
 `use strict`;
 const rollOutput = document.getElementById("rollOutput");
 
-// const
-
-function refresh4() {
-    const rndInt = rollDice(4);
-    console.log(rndInt);
-    location.reload();
-}
-
-function refresh6() {
-    const rndInt = rollDice(6);
-    console.log(rndInt);
-    location.reload();
-}
-
 function rollDice(max) {
-    let roll = Math.floor(Math.random() * (max)) + 1
+    let roll = Math.floor(Math.random() * (max) + 1)
     rollOutput.innerText = roll;
+    console.log(roll)
     // container.appendChild(row).className = "row-container";
 }
 
+// roll test
 
-// setTimeout(() => {
-//     document.location.reload();
-// }, 3000);
-
-
-// function rollDice(rows, cols) {
-//     for (let i = 0; i < (rows); i++) {
-//         let row = document.createElement("div");
-//         // row.innerText = i + 1;
-//         container.appendChild(row).className = "row-container";
-//
-//         for (let j = 0; j < (cols); j++) {
-//             let cell = document.createElement("div");
-//             // cell.innerText = j + 1;
-//             row.appendChild(cell).className = "grid-item";
-//         }
-//     }
-// }
-//
-// makeGrid(6, 8);
+function refresh4() {
+    for (let i = 0; i < 10; i++) {
+        rollDice(4);
+    }
+    // const rndInt = rollDice(4);
+    // console.log(rndInt);
+    // location.reload();
+}

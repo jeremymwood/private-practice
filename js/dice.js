1a8ceafc4e6cc3aab87bf51c83def7aa5d4cd2c1
Jeremy Wood <jeremy.mason.wood@gmail.com>
2022-12-20 21:48:28 -0600

    221220-1851-NaN-onclick

--- js/dice.js
+++ js/dice.js
@@ -8,23 +8,6 @@ const diceArray = [ 4, 6, 8, 10, 12, 20];
 
 const dice = {};
 
-for (const die of diceArray) {
-
-    const button = optionContainer.appendChild(document.createElement('button'));
-
-    dice[die] = new function() {
-        this.name = `D`+ [die];
-        this.value = die;
-        button.className = this.name;
-        button.setAttribute(`value`,`Refresh Webpage.`);
-        button.setAttribute(`onclick`,`refresh` + this.value + `()`);
-        button.innerText = this.name;
-    }
-}
-
-
-// console.log(dice);
-
 function rollDice(max) {
     const roll = Math.floor(Math.random() * (max) + 1)
     rollOutput.innerText = `${roll}`;
@@ -38,49 +21,45 @@ function rollDice(max) {
     // }
 }
 
-// function refresh(dice) {
-//     rollDice(dice.value);
+for (const die of diceArray) {
+
+    const button = optionContainer.appendChild(document.createElement('button'));
+
+    dice[die] = new function () {
+        this.name = `D` + [die];
+        this.value = die;
+        button.className = this.name;
+        button.setAttribute(`value`, `Refresh Webpage.`);
+        button.setAttribute(`onclick`, `refresh()`);
+        button.innerText = this.name;
+    }
+}
+function refresh(die) {
+    rollDice(this.value);
+}
+
+console.log(refresh(4));
+
+// function refresh4() {
+//     rollDice(4);
 // }
 //
-// refresh(4);
-
-// for ( die of diceArray) {
-//
-//     function refresh() {
-//         rollDice(this.value);
-//
-//     // dice[die] = new function() {
-//     //     this.name = `D`+ [die];
-//     //     this.value = die;
-//     //     button.className = this.name;
-//     //     button.setAttribute(`value`,`Refresh Webpage.`);
-//     //     button.setAttribute(`onclick`,`refresh()`);
-//     //     button.innerText = this.name;
-//     // }
+// function refresh6() {
+//     rollDice(6);
 // }
-
-
-
-function refresh4() {
-    rollDice(4);
-}
-
-function refresh6() {
-    rollDice(6);
-}
-
-function refresh8() {
-    rollDice(8);
-}
-
-function refresh10() {
-    rollDice(10);
-}
-
-function refresh12() {
-    rollDice(12);
-}
-
-function refresh20() {
-    rollDice(20);
-}
+//
+// function refresh8() {
+//     rollDice(8);
+// }
+//
+// function refresh10() {
+//     rollDice(10);
+// }
+//
+// function refresh12() {
+//     rollDice(12);
+// }
+//
+// function refresh20() {
+//     rollDice(20);
+// }

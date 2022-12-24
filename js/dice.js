f0f6989a929904d819c52d0c2da1819535e254e6
Jeremy Wood <jeremy.mason.wood@gmail.com>
2022-12-24 11:33:54 -0600

    221224-1133-modified

--- js/dice.js
+++ js/dice.js
@@ -3,13 +3,15 @@ const rollOutput = document.getElementById("rollOutput");
 
 const optionContainer = document.getElementById("optionContainer");
 
-const diceArray = [ 4, 6, 8, 10, 12, 20, 100, 1000];
+const diceArray = [ 4, 6, 8, 10, 12, 20];
+
+// , 100, 1000, 10000, 100000, 1000000, 10000000
 
 const dice = {};
 
 function rollDice(max) {
     const roll = Math.floor(Math.random() * (max) + 1)
-    rollOutput.innerText = `${roll}`;
+    rollOutput.innerText = `${roll.toLocaleString("en-US")}`;
     // check random
     // for (let i = 0; i < 10; i++) {
     //     let roll = Math.floor(Math.random() * (max) + 1)
@@ -24,9 +26,8 @@ for (const die of diceArray) {
     const button = optionContainer.appendChild(document.createElement('button'));
 
     dice[die] = new function () {
-        this.name = `D` + [die];
+        this.name = `D` + [die.toLocaleString("en-US")];
         this.value = die;
-        button.setAttribute(`value`, `Refresh Webpage.`);
         button.innerText = this.name;
         button.addEventListener("click", function() {
             rollDice(die);

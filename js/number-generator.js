4cfbdb9f606a2fc812ed48c8cef4b7232e4320a8
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-01-09 14:36:16 -0600

    220109-1436

---
+++ js/number-generator.js
@@ -0,0 +1,11 @@
+function makeid(length) {
+    let result           = '';
+    let characters       = '0123456789';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+console.log(makeid(6));

1b809f5577683aca8e46a58145340781f066f22b
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-01-09 21:43:15 -0600

    220109-2143-mostly working

--- js/number-generator.js
+++ js/number-generator.js
@@ -1,13 +1,31 @@
+
 function makeid(length) {
-    let result           = '';
-    let characters       = '0123456789';
+    let result = '';
+    let characters = '0123456789';
     let charactersLength = characters.length;
     for ( let i = 0; i < length; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+
+    // prints iterating groups of unit block
+    // console.log(result);
     }
     return result;
 }
 
-console.log(makeid(6));
+// prints block
+// console.log(makeid(6));
 
+const blockLength = 6;
+const finalProductLength = 10;
+
+function finalProduct(length) {
+    let result = '';
+    for ( let i = 0; i < length; i++ ) {
+        result += makeid(blockLength) + " ";
+    }
+    return result;
+}
+
+
+console.log(finalProduct(finalProductLength));
 // append collection until 2400 characters including spaces met

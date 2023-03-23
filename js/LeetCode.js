3723b6b74125dfed347a6378698b80daedeffc13
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-23 08:55:16 -0500

    0855

--- js/LeetCode.js
+++ js/LeetCode.js
@@ -1,22 +1,29 @@
 'use strict';
 
+// const twoSum = function(nums, target) {
+//     let numsArr = [];
+//     let newArr = [];
+//     for (let i = 0; i < nums.length; i++) {
+//         numsArr.push(nums[i]);
+//         newArr.push(i);
+//         let numsSum = numsArr.reduce((a, b) => a + b, 0);
+//         if (numsSum === target) {
+//             console.log(newArr);
+//             break;
+//         }
+//     }
+// };
+
+
 const twoSum = function(nums, target) {
-    let numsArr = [];
-    let newArr = [];
-    for (let i = 0; i < nums.length; i++) {
-        numsArr.push(nums[i]);
-        // console.log(numsArr);
-        newArr.push(i);
-        // console.log(newArr);
-        let numsSum = numsArr.reduce((a, b) => a + b, 0);
-        // console.log(numsSum);
-        if (numsSum === target) {
-            console.log(newArr);
-            // return newArr;
-            break;
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i+1; j < nums.length; j++){
+            if(nums[i] + nums[j] === target){
+                console.log([i, j]);
+                return [i, j]
+            }
         }
     }
-    console.log("New array ${newArr}");
 };
 
 // twoSum([1,2,3], 3);

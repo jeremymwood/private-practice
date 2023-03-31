83f8c91aaa29ebec53dc8aeae4cd5169dadc3550
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-31 16:17:07 -0500

    1616-input success

--- js/grid.js
+++ js/grid.js
@@ -1,26 +1,26 @@
 
-let rows = 2;
+let rows = 0;
 let cols = 18;
 
 function makeGrid() {
     const container = document.getElementById("container");
 
-    for (let i = 0; i < (rows); i++) {
-        let row = document.createElement("div");
-        // row.innerText = i + 1;
-        container.appendChild(row).className = "row-container";
+        for (let i = 0; i < (rows); i++) {
+            let row = document.createElement("div");
+            // row.innerText = i + 1;
+            container.appendChild(row).className = "row-container";
 
-        for (let j = 0; j < (cols); j++) {
-            let cell = document.createElement("button");
+            for (let j = 0; j < (cols); j++) {
+                let cell = document.createElement("button");
 
-            // cell.innerText = j + 1;
-            row.appendChild(cell).className = "grid-item";
-            // );
+                // cell.innerText = j + 1;
+                row.appendChild(cell).className = "grid-item";
+                // );
+            }
         }
-    }
     // document.getElementById(container).innerHTML = fcHtmlModal;
-
 }
+
 makeGrid();
 
 //submit connected
@@ -29,5 +29,9 @@ $('.btn').click(function () {
     let value = $("#rowInput").val();
     console.log(value);
     rows = value;
-    makeGrid();
+
+    if ($(`#container`).find(`.row-container`) !== null) {
+        $(`#container`).find(`.row-container`).remove();
+        makeGrid();
+    }
 });

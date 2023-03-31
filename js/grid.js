c09a743ece961c6217efdac774b841ae425daf6f
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-31 16:53:55 -0500

    1653-row-col-inputs-wkg

--- js/grid.js
+++ js/grid.js
@@ -1,43 +1,35 @@
+let rows = 6;
+let cols = 16;
 
-let rows = 5;
-let cols = 18;
-
-function makeGrid() {
+makeGrid = function () {
     const container = document.getElementById("container");
 
-        for (let i = 0; i < (rows); i++) {
-            let row = document.createElement("div");
-            // row.innerText = i + 1;
-            container.appendChild(row).className = "row-container";
+    for (let i = 0; i < (rows); i++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "row-container";
 
-            for (let j = 0; j < (cols); j++) {
-                let cell = document.createElement("button");
-
-                // cell.innerText = j + 1;
-                row.appendChild(cell).className = "grid-item";
-                // );
-            }
+        for (let j = 0; j < (cols); j++) {
+            let cell = document.createElement("button");
+            row.appendChild(cell).className = "grid-item";
         }
-    // document.getElementById(container).innerHTML = fcHtmlModal;
+    }
 }
 
 makeGrid();
 
-//submit connected
-//adds rows, but soes not replace
-$(document).keyup(function(event) {
-    if ($(`#rowInput`).is(":focus") && event.key === "Enter") {
-        rowUpdate ();
-    }
-});
-
-function rowUpdate () {
-    let value = $("#rowInput").val();
-    console.log(value);
-    rows = value;
+gridUpdate = function () {
+    let rowValue = $("#rowInput").val();
+    let colValue = $("#colInput").val();
+    rows = rowValue;
+    cols = colValue;
 
     if ($(`#container`).find(`.row-container`) !== null) {
-        $(`#container`).find(`.row-container`).remove();
+        $(`.row-container`).remove();
         makeGrid();
     }
 };
+$(document).keyup(function(event) {
+    if ($(`#rowInput`).is(":focus") || $(`#colInput`).is(":focus") && event.key === "Enter") {
+        gridUpdate ();
+    }
+});

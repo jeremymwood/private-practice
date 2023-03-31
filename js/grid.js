b44ef8b6e9d8b5c0b3818878ddcb96acf44f7506
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-30 22:17:13 -0500

    2217

--- js/grid.js
+++ js/grid.js
@@ -1,4 +1,8 @@
+
+
 const container = document.getElementById("container");
+
+const rowInput = document.getElementById("rowInput");
 
 function makeGrid(rows, cols) {
     for (let i = 0; i < (rows); i++) {
@@ -14,9 +18,32 @@ function makeGrid(rows, cols) {
             // );
         }
     }
+    // document.getElementById(modalOptionsId).innerHTML = fcHtmlModal;
+
 }
+
 // let cell = document.createElement("button");
 // // work on depressed state
 // cell.addEventListener("click", cell.appendChild().className = "grid-selected";)
 
 makeGrid(6, 18);
+
+//submit connected
+$('.btn').click(function () {
+    var value = $("#rowInput").val();
+    console.log(value);
+});
+
+
+// function myFunction() {
+//     var x = document.getElementById("myNumber").value;
+//     document.getElementById("demo").innerHTML = x;
+// }
+//
+// function returnRow(){
+//     let input = document.getElementById("rowInput").value;
+//     // alert(input);
+//     console.log(input);
+// }
+// returnRow();
+// makeGrid(returnRow(), 18);

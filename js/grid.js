6d840861d8fe3ca2748fd17634045d617a0c4429
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-04-01 00:01:04 -0500

    0000-row-input-btns

--- js/grid.js
+++ js/grid.js
@@ -4,11 +4,11 @@ let cols = 16;
 makeGrid = function () {
     const container = document.getElementById("container");
 
-    for (let i = 0; i < (rows); i++) {
+    for (let i = 0; i < rows; i++) {
         let row = document.createElement("div");
         container.appendChild(row).className = "row-container";
 
-        for (let j = 0; j < (cols); j++) {
+        for (let j = 0; j < cols; j++) {
             let cell = document.createElement("button");
             row.appendChild(cell).className = "grid-item";
         }
@@ -33,3 +33,29 @@ $(document).keyup(function(event) {
         gridUpdate ();
     }
 });
+
+const rowQ = 10;
+const colQ = 10;
+makeRowSelector = function () {
+    const selectors = document.getElementById("selectors");
+    let rowInput = document.createElement("div");
+    let colInput = document.createElement("div");
+    selectors.appendChild(rowInput).className = "row-selector-container";
+    selectors.appendChild(colInput).className = "col-selector-container";
+
+    for (let i = 0; i < rowQ; i++) {
+        let cell = document.createElement("button");
+        // cell.innerText(i+1);
+        // $(`${cell}`).innerText(i+1)
+        rowInput.appendChild(cell).className = "grid-item";
+    }
+
+    // for (let j = 0; j < colQ; j++) {
+    //     let cell = document.createElement("button");
+    //     cell.innerHTML(j+1);
+    //     colInput.appendChild(cell).className = "grid-item";
+    // }
+
+
+}
+makeRowSelector ();

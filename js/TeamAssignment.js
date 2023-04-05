bc12961bdbcb601d51c4799e9c9bc2c063e96234
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-04-05 08:48:20 -0500

    0848-refactor

--- js/TeamAssignment.js
+++ js/TeamAssignment.js
@@ -1,40 +1,40 @@
 'use strict';
 
-const voters = 6;
-let people = [];
+const voterPool = 5;
+let voters = [];
 
-const personMachina = function() {
-    for (let i = 0; i < voters; i++) {
-        let person = `Voter ${i + 1}`;
-        people.push(person);
+const voterMachina = function() {
+    for (let i = 0; i < voterPool; i++) {
+        let voter = `Voter ${i + 1}`;
+        voters.push(voter);
     }
     console.log("");
-    console.log(`Voters: [${people}]`);
+    console.log(`Voters: [${voters}]`);
     console.log("");
 
 }
 
 /* Randomize array in-place using Durstenfeld shuffle algorithm */
 const castVotes = function () {
-    for (let i = 0; i < people.length; i++) {
-        let tempPeople = [];
+    for (let i = 0; i < voters.length; i++) {
+        let tempVoters = [];
 
-        for (let j = 0; j < people.length; j++) {
+        for (let j = 0; j < voters.length; j++) {
             if (j !== i) {
-                tempPeople.push(people[j]);
+                tempVoters.push(voters[j]);
             }
         }
 
-        for (let j = tempPeople.length - 1; j > 0; j--) {
+        for (let j = tempVoters.length - 1; j > 0; j--) {
             let k = Math.floor(Math.random() * (j + 1));
-            if (tempPeople[j] !== tempPeople[k]) {
-                let temp = tempPeople[j];
-                tempPeople[j] = tempPeople[k];
-                tempPeople[k] = temp;
+            if (tempVoters[j] !== tempVoters[k]) {
+                let temp = tempVoters[j];
+                tempVoters[j] = tempVoters[k];
+                tempVoters[k] = temp;
             }
         }
-        console.log(`Voter ${i + 1} votes: [${tempPeople}]`);
+        console.log(`Voter ${i + 1} votes: [${tempVoters}]`);
     }
 }
-personMachina();
+voterMachina();
 castVotes();

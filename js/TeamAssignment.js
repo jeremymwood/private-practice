b1fa3b5d7006db43e1912359a12ce0616a79bb48
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-04-06 22:02:07 -0500

    2202

--- js/TeamAssignment.js
+++ js/TeamAssignment.js
@@ -9,47 +9,40 @@ const voterMachina = function() {
         voters[i]  = {
             name: `Voter ${i + 1}`
         };
-        // console.log(voters[i].name);
     }
-    // console.log("");
-    // console.log(`Voters: [${voters.name}]`);
-    // console.log(voters);
-    // console.log("");
-
 }
 
 /* Randomize array in-place using Durstenfeld shuffle algorithm */
 const castVotes = function () {
     for (let i = 0; i < voters.length; i++) {
-        let tempVoters = [];
+        let voterVotes = [];
 
         for (let j = 0; j < voters.length; j++) {
             if (j !== i) {
-                tempVoters.push(voters[j]);
+                voterVotes.push(voters[j].name.toString());
             }
         }
 
-        for (let j = tempVoters.length - 1; j > 0; j--) {
+        for (let j = voterVotes.length - 1; j > 0; j--) {
             let k = Math.floor(Math.random() * (j + 1));
-            if (tempVoters[j] !== tempVoters[k]) {
-                let temp = tempVoters[j];
-                tempVoters[j] = tempVoters[k];
-                tempVoters[k] = temp;
+            if (voterVotes[j] !== voterVotes[k]) {
+                let temp = voterVotes[j];
+                voterVotes[j] = voterVotes[k];
+                voterVotes[k] = temp;
             }
         }
 
-        voters[i].votes = tempVoters[i];
-
-        // const tempVotes = [...new Set(tempVoters.flatMap(x=>[x.name]))]
-        // console.log(`${voters[i].name} votes: [${tempVotes}]`);
-
-        // console.log(`${voters[i].name} votes: [${tempVoters[i].name}]`);
-        // console.log(tempVoters[i].name);
+        voters[i].votes = voterVotes;
+        // console.log(voters[i]);
         // console.log(voterVotes[i]);
-        // voters[i].votes = tempVoters
-
-        // console.log(voters[i].votes)
+        // console.log(voters[i].name);
+        // console.log(voters[i].votes);
+        console.log(`${voters[i].name} votes: [${voters[i].votes}]`);
     }
+    // console.log(voters);
 }
 voterMachina();
 castVotes();
+
+//todo
+//many to many top vote

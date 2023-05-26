f2efbd146da3c7260049e7167869032f5bb2e15a
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-05-26 11:01:02 -0500

    plusMinus

--- js/hackerRank.js
+++ js/hackerRank.js
@@ -43,32 +43,53 @@
 //
 // aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]);
 
-function diagonalDifference(arr) {
-    let a = 0;
-    let b = 0;
-    let backwards = arr.length -1;
-
-    for (let i = 0; i < arr.length; i++) {
-        a += arr[i][i];
-        b += arr[i][backwards];
-        console.log(backwards);
-        console.log(`a = ${a}`);
-        console.log(`b = ${b}`);
-
-        backwards -= 1;
-    }
-
-    return Math.abs(a - b);
-}
-
-
-console.log(diagonalDifference([
-    [11, 2, 4],
-    [4, 5, 6],
-    [10, 8, -12]
-]))
-// diagonalDifference([
+// function diagonalDifference(arr) {
+//     let a = 0;
+//     let b = 0;
+//     let backwards = arr.length -1;
+//
+//     for (let i = 0; i < arr.length; i++) {
+//         a += arr[i][i];
+//         b += arr[i][backwards];
+//         console.log(backwards);
+//         console.log(`a = ${a}`);
+//         console.log(`b = ${b}`);
+//
+//         backwards -= 1;
+//     }
+//
+//     return Math.abs(a - b);
+// }
+//
+//
+// console.log(diagonalDifference([
 //     [11, 2, 4],
 //     [4, 5, 6],
 //     [10, 8, -12]
-// ]);
+// ]))
+
+function plusMinus(arr) {
+    let total = arr.length;
+    let pos = 0;
+    let neg = 0;
+    let zero = 0;
+    for (let i = 0; i < total; i++) {
+        if (arr[i] > 0) {
+            pos += 1;
+        }
+        if (arr[i] < 0) {
+            neg += 1;
+        }
+        if (arr[i] === 0) {
+            zero += 1;
+        }
+    }
+    let posR = (pos / total).toFixed(6);
+    let negR = (neg / total).toFixed(6);
+    let zeroR = (zero / total).toFixed(6);
+    console.log(`${posR}`);
+    console.log(`${negR}`);
+    console.log(`${zeroR}`);
+}
+
+plusMinus([1,1,0,-1,-1]);
